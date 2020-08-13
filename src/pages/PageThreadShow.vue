080dab5e0d87323770a303453fb9a89faec9343b
bryan-m-rojas <bmr333@nyu.edu>
2020-08-12 20:03:39 -0400

    let discussion begin

--- src/pages/PageThreadShow.vue
+++ src/pages/PageThreadShow.vue
@@ -2,6 +2,25 @@
   <div class="col-large push-top">
     <h1>{{thread.title}}</h1>
     <PostList :posts="posts"/>
+
+    <form @submit.prevent="addPost">
+      <div class="form-group">
+        <textarea 
+          name="" 
+          id="" 
+          cols="30" 
+          rows="10" 
+          class="form-input"
+          v-model="newPostText"
+        >
+        </textarea>
+      </div>
+      <div class="form-actions">
+        <button class="btn-blue">
+          Submit Post
+        </button>
+      </div>
+    </form>
   </div>
 </template>
 
@@ -23,7 +42,8 @@
 
     data () {
       return {
-        thread: sourceData.threads[this.id]
+        thread: sourceData.threads[this.id],
+        newPostText: ''
       }
     },
 
@@ -32,6 +52,25 @@
         const postIds = Object.values(this.thread.posts)
         return Object.values(sourceData.posts)
           .filter(post => postIds.includes(post['.key']))
+      }
+    },
+
+    methods: {
+      addPost () {
+        const postId = 'greatPost' + Math.random()
+        const post = {
+          text: this.newPostText,
+          publishedAt: Math.floor(Date.now() / 1000),
+          threadId: this.id,
+          userId: 'jUjmgCurRRdzayqbRMO7aTG9X1G2',
+          '.key': postId
+        }
+
+        // $set params ->  Object we want to add new property to/ name of property/ value
+        this.$set(sourceData.posts, postId, post)
+        this.$set(this.thread.posts, postId, postId)
+        this.$set(sourceData.users[post.userId].posts, postId, postId)
+        this.newPostText = ''
       }
     }
   }
