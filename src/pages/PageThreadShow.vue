7441ad17eb45949f92aa8192d8012d2762427134
bryan-m-rojas <bmr333@nyu.edu>
2020-08-12 22:31:19 -0400

    communicate between components

--- src/pages/PageThreadShow.vue
+++ src/pages/PageThreadShow.vue
@@ -2,35 +2,23 @@
   <div class="col-large push-top">
     <h1>{{thread.title}}</h1>
     <PostList :posts="posts"/>
-
-    <form @submit.prevent="addPost">
-      <div class="form-group">
-        <textarea 
-          name="" 
-          id="" 
-          cols="30" 
-          rows="10" 
-          class="form-input"
-          v-model="newPostText"
-        >
-        </textarea>
-      </div>
-      <div class="form-actions">
-        <button class="btn-blue">
-          Submit Post
-        </button>
-      </div>
-    </form>
+    <PostEditor 
+      @save="addPost"
+      :threadId="id"
+    />
   </div>
 </template>
 
 <script>
   import sourceData from '@/data'
+  import PostEditor from '@/components/PostEditor'
   import PostList from '@/components/PostList'
+  
   export default {
     name: 'PageThreadShow',
     components: {
-      PostList
+      PostList,
+      PostEditor
     },
 
     props: {
@@ -56,21 +44,14 @@
     },
 
     methods: {
-      addPost () {
-        const postId = 'greatPost' + Math.random()
-        const post = {
-          text: this.newPostText,
-          publishedAt: Math.floor(Date.now() / 1000),
-          threadId: this.id,
-          userId: 'jUjmgCurRRdzayqbRMO7aTG9X1G2',
-          '.key': postId
-        }
+      addPost (eventData) {
+        const postId = eventData.post['key']
+        const post = eventData.post
 
         // $set params ->  Object we want to add new property to/ name of property/ value
         this.$set(sourceData.posts, postId, post)
         this.$set(this.thread.posts, postId, postId)
         this.$set(sourceData.users[post.userId].posts, postId, postId)
-        this.newPostText = ''
       }
     }
   }
