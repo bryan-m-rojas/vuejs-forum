21ea44bc230dd125f99281b94bbba140ba3af6c4
bryan-m-rojas <bmr333@nyu.edu>
2020-08-17 19:16:32 -0400

    Forum COmponents

---
+++ src/components/ForumList.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="forum-list">
+    <h2 class="list-title">
+      <a href="#">Feedback &amp; Information</a>
+    </h2>
+
+    <ForumListItem
+      v-for="forum in forums"
+      :forum="forum"
+      :key="forum['.key']"   
+    />
+  </div>
+</template>
+
+<script>
+  import ForumListItem from '@/components/ForumListItem'
+  export default {
+    props: {
+      components: {
+        ForumListItem
+      },
+    
+      forums: {
+        required: true,
+        type: Array
+      }
+    }
+  }
+</script>
+
+<style scoped>
+</style>
