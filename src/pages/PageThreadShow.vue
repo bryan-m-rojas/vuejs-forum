323f52d3e87a1f0b0bc7e751ddb02074886d14cc
bryan-m-rojas <bmr333@nyu.edu>
2020-08-19 16:22:16 -0400

    Creating Posts using Vuex

--- src/pages/PageThreadShow.vue
+++ src/pages/PageThreadShow.vue
@@ -6,10 +6,7 @@
       <span style="float:right; margin-top: 2px;" class="hide-mobile text-faded text-small">3 replies by 3 contributors</span>
     </p>
     <PostList :posts="posts"/>
-    <PostEditor 
-      @save="addPost"
-      :threadId="id"
-    />
+    <PostEditor :threadId="id"/>
   </div>
 </template>
 
@@ -43,17 +40,6 @@
         const postIds = Object.values(this.thread.posts)
         return Object.values(this.$store.state.posts)
           .filter(post => postIds.includes(post['.key']))
-      }
-    },
-
-    methods: {
-      addPost ({post}) {
-        const postId = post['.key']
-
-        // $set params ->  Object we want to add new property to/ name of property/ value
-        this.$set(this.$store.state.posts, postId, post)
-        this.$set(this.thread.posts, postId, postId)
-        this.$set(this.$store.state.users[post.userId].posts, postId, postId)
       }
     }
   }
