5fb7417917ce067c12bf82cdc399ff52260ff57c
bryan-m-rojas <bmr333@nyu.edu>
2020-08-24 19:03:55 -0400

    Update threads in Firebase Realtime Database

--- src/store/actions.js
+++ src/store/actions.js
@@ -12,11 +12,11 @@ export default {
     updates[`threads/${post.threadId}/contributors/${post.userId}`] = post.userId
     updates[`users/${post.userId}/posts/${postId}`] = postId
 
-    firebase.database.ref().update(updates)
+    firebase.database().ref().update(updates)
       .then(() => {
         commit('setPost', {post, postId})
         commit('appendPostToThread', {parentId: post.threadId, childId: postId})
-        commit('appendContributorsToThread', {parentId: post.threadId, childId: post.userId})
+        commit('appendContributorToThread', {parentId: post.threadId, childId: post.userId})
         commit('appendPostToUser', {parentId: post.userId, childId: postId})
         return Promise.resolve(state.posts[postId])
       })
@@ -60,12 +60,23 @@ export default {
   updateThread ({state, commit, dispatch}, {title, text, id}) {
     return new Promise((resolve, reject) => {
       const thread = state.threads[id]
-      const newThread = {...thread, title}
-      commit('setThread', {thread: newThread, threadId: id})
+      const post = state.posts[thread.firstPostId]
 
-      dispatch('updatePost', {id: thread.firstPostId, text})
+      const edited = {
+        at: Math.floor(Date.now() / 1000),
+        by: state.authId
+      }
+
+      const updates = {}
+      updates[`posts/${thread.firstPostId}/text`] = text
+      updates[`posts/${thread.firstPostId}/edited`] = edited
+      updates[`threads/${id}/title`] = title
+
+      firebase.database().ref().update(updates)
         .then(() => {
-          resolve(newThread)
+          commit('setThread', {thread: {...thread, title}, threadId: id})
+          commit('setPost', {postId: thread.firstPostId, post: {...post, text, edited}})
+          resolve(post)
         })
     })
   },
