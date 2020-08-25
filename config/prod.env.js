97356bd9f34f91df3afdfec203508a358be33e0c
bryan-m-rojas <bmr333@nyu.edu>
2020-08-25 18:25:25 -0400

    Using Environment Variables in the Vue.js Webpack Template

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,12 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"'
+  FIREBASE_NODE_ENV: '"production"',
+  FIREBASE_API_KEY: "'AIzaSyDcp9qXbKh8695-I6wQGlYex-RLNjLuCXU'",
+  FIREBASE_AUTH_DOMAIN: "'vue-master-class-forum.firebaseapp.com'",
+  FIREBASE_DATABASE_URL: "'https://vue-master-class-forum.firebaseio.com'",
+  FIREBASE_PROJECT_ID: "'vue-master-class-forum'",
+  FIREBASE_STORAGE_BUCKET: "'vue-master-class-forum.appspot.com'",
+  FIREBASE_MESSAGING_ID: "'1049376509855'",
+  FIREBASE_APP_ID: "'1:1049376509855:web:abd36980029af558fff7aa'",
+  FIREBASE_MEASUREMENT_ID: "'G-DHTLXS1BS3'"
 }
