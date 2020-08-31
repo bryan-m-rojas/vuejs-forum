0c011267dc8ca4121f97823b9fbdd3d7c02afb74
bryan-m-rojas <bmr333@nyu.edu>
2020-08-31 16:45:05 -0400

    auth fix

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -54,7 +54,7 @@ export default {
           const user = data.user
           firebase.database().ref('users').child(user.uid).once('value', snapshot => {
             if (!snapshot.exists()) {
-              return dispatch('users/createUser', {id: user.user.uid, name: user.displayName, email: user.email, username: user.email, avatar: user.photoURL}, {root: true})
+              return dispatch('users/createUser', {id: user.uid, name: user.displayName, email: user.email, username: user.email, avatar: user.photoURL}, {root: true})
                 .then(() => dispatch('fetchAuthUser'))
             }
           })
